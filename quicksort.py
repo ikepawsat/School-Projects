0c361fdad8da4cbde0ea88db3cb9c2366e209094
ikepawsat <ike.pawsat@gmail.com>
2024-08-20 15:51:54 -0500

    Practicing

--- quicksort.py
+++ quicksort.py
@@ -2,42 +2,38 @@ import random
 
 def pivot(arr, low, high):
     i = low - 1
-    pi = arr[high]
-
     for x in range(low, high):
-        if arr[x] < pi:
+        if arr[x] < arr[high]:
             i += 1
             arr[i], arr[x] = arr[x], arr[i]
 
     i += 1
     arr[i], arr[high] = arr[high], arr[i]
     return i
-        
 
-def quick_sort(arr, low, high):
+def quicksort(arr, low, high):
     if low < high:
         piv = pivot(arr, low, high)
 
-        quick_sort(arr, low, piv - 1)
-        quick_sort(arr, piv + 1, high)
+        quicksort(arr, low, piv - 1)
+        quicksort(arr, piv + 1, high)
 
 def binary_search(arr, target):
-    low = 0 
+    low = 0
     high = len(arr) - 1
-
     while low <= high:
-        middle = (low + high) // 2
+        middle = (high + low) // 2
         if arr[middle] < target:
             low = middle + 1
         elif arr[middle] > target:
             high = middle - 1
         else:
             return middle
-        
+    
     return -1 #not found
 
-arr = [random.randint(1, 1000) for _ in range(100)]
-quick_sort(arr, 0, len(arr) - 1)
+arr = [random.randint(1, 999) for _ in range(1000)]
+quicksort(arr, 0, len(arr) - 1)
 print(arr)
-target = arr[random.randint(0, len(arr) - 1)]
-print(f"{target} found at index {binary_search(arr, target)}")
+target = random.randint(1, 999)
+print(f"{target} found at position {binary_search(arr, target)}")
